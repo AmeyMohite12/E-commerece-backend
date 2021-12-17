f94cf5d3abdf932406a0293fd8d7f26c5266c5b4
Ameya <ameymohite99@gmail.com>
2021-12-17 19:47:14 +0530

    Added configuration to expose Ids of products in rest for dynamic category load on UI.

--- src/main/java/com/ameyecommerce/springbootecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/ameyecommerce/springbootecommerce/dao/ProductCategoryRepository.java
@@ -3,10 +3,12 @@ package com.ameyecommerce.springbootecommerce.dao;
 import com.ameyecommerce.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // overriding the defaults of RestRepo , productCatepgry = JSON entity name and path = /product-category
 // normally rest repo will just add an S to whatever the entity name i.e. /productcategorys
 
+@CrossOrigin("*")
 @RepositoryRestResource(collectionResourceRel =  "productCategory" , path = "product-category")
 public interface ProductCategoryRepository  extends JpaRepository<ProductCategory , Long> {
 
