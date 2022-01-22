c4a7f654892258a3d7478b593d2f9001126f600b
Ameya <ameymohite99@gmail.com>
2022-01-22 15:16:41 +0530

    Added api end points for fetching country and states information and refactored http methods for classes.

--- src/main/java/com/ameyecommerce/springbootecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/ameyecommerce/springbootecommerce/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package com.ameyecommerce.springbootecommerce.config;
 
+import com.ameyecommerce.springbootecommerce.entity.Country;
 import com.ameyecommerce.springbootecommerce.entity.Product;
 import com.ameyecommerce.springbootecommerce.entity.ProductCategory;
+import com.ameyecommerce.springbootecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -37,20 +39,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        disableHTTPMethods(Product.class ,config, theUnsupportedActions);
+        disableHTTPMethods(ProductCategory.class ,config, theUnsupportedActions);
+        disableHTTPMethods(State.class ,config, theUnsupportedActions);
+        disableHTTPMethods(Country.class ,config, theUnsupportedActions);
 
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
         // need to expose Ids of product category in ProductCategory repo so need this helper method with entity manager object.
         exposeIds(config);
 
+    }
+
+    private void disableHTTPMethods(Class theClass ,RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config){
